95258691f8d5facea644880ffe388f4a6740227f
Bradley Taunt <bradleytaunt@gmail.com>
2018-12-21 13:46:28 -0500

    Add fonts folder to eleventy setup

--- .eleventy.js
+++ .eleventy.js
@@ -61,6 +61,7 @@ module.exports = function (eleventyConfig) {
 
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("static/images");
+  eleventyConfig.addPassthroughCopy("static/fonts");
 
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
